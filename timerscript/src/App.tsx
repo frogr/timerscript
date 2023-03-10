808b855ec759be47374820a1244527acc2f16836
austin <austindanielfrench@gmail.com>
2023-03-09 21:44:45 -0800

    idk what happened but we have set up the files git push

---
+++ timerscript/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Timer from './timer';
+
+function App() {
+  return (
+    <div>
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
---
+++ timerscript/src/timer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+
+export default class Timer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>TWO TIMERS</h1>
+            </div>
+        )
+    }
+}
